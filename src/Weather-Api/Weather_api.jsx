d979f8f82ad455ba1df825b721593100b4e661ff
Urinfinite8171 <shashiraj2468@gmail.com>
2025-01-25 23:04:31 +0530

    aws public ip address

--- src/Weather-Api/Weather_api.jsx
+++ src/Weather-Api/Weather_api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REST_API_URL = "http://localhost:8080";
+const REST_API_URL = "http://3.110.167.178:8080";
 
 export const weather_api = (ip) => {
   return axios.get(REST_API_URL + "/weather/api/weather-by-ip", {
