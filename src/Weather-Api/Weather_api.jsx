badc16da41031c0c6eb55ad77a98d4c3560c68d3
Urinfinite8171 <shashiraj2468@gmail.com>
2025-01-25 20:26:20 +0530

    ec2 instance ip address is added

--- src/Weather-Api/Weather_api.jsx
+++ src/Weather-Api/Weather_api.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const REST_API_URL = "http://localhost:8082";
+const REST_API_URL =
+  "http://ec2-65-2-153-2.ap-south-1.compute.amazonaws.com:8080";
 
 export const weather_api = (ip) => {
   return axios.get(REST_API_URL + "/weather/api/weather-by-ip", {
